70253a0cdcf0bb68920e9f7d5ab76845602e49fc
Mohamed Araby <mohamedaraby1296@gmail.com>
2022-04-13 09:04:42 +0200

    _1

---
+++ Assets/_Scripts/ButtonBehaviours.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviours : MonoBehaviour
+{
+    public GameObject[] ToHideObjects;
+    public GameObject[] ToShowObjects;
+
+    public void ClickedIT(){
+        foreach(GameObject i in ToHideObjects){i.SetActive(false);}
+        foreach(GameObject i in ToShowObjects) {i.SetActive(true);}
+        }
+
+}
---
+++ Assets/_Scripts/MyMainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyMainMenu : MonoBehaviour
+{
+    public string nameKid;
+    public int age;
+
+    public Text kidName_1;
+    public Text kidName_2;
+    public void SetAge(int agee)
+    {
+        age = agee;
+        PlayerPrefs.SetInt("age", age);
+    }
+    public void SetName(InputField fields)
+    {
+        nameKid = fields.text;
+        PlayerPrefs.SetString("name", nameKid);
+        UpdateUI();
+    }
+    public void UpdateUI()
+    {
+        kidName_1.text = nameKid;
+        kidName_2.text = nameKid;
+    }
+}
