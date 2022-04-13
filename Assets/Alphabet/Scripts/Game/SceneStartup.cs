a6833360fca23ce61237dab5b0cd982aa882217a
Mohamed Araby <mohamedaraby1296@gmail.com>
2022-04-13 06:42:08 +0200

    Initial commit

---
+++ Assets/Alphabet/Scripts/Game/SceneStartup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using alphabet;
+
+///Developed by Indie Studio
+///https://www.assetstore.unity3d.com/en/#!/publisher/9268
+///www.indiestd.com
+///info@indiestd.com
+
+public class SceneStartup : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+
+
+	void OnDestroy ()
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Badrawy Productions
+ 
